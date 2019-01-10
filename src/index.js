6e2505ce42a33a93f90cc712a7d0bfd68bf96be0
songofhawk <songofhawk@gmail.com>
2019-01-10 16:15:44 +0800

    处理warning

--- src/index.js
+++ src/index.js
@@ -7,17 +7,7 @@ let hello = function () {
 
 hello();
 
-//测试fabric引用
-let canvas = new fabric.Canvas(document.querySelector('#image-area'), {
-    selection: false,   //按照官方文档,是禁止了group selection
-    width:600,
-    height:600,
-    hoverCursor:'pointer'
-});
 
-
-console.log('canvas initialized!');
-
-import ImageLabelTool from './image_label/ImageLabelTool.js';
-let labelTool = new ImageLabelTool();
+import {ImageLabelTool} from './image_label/ImageLabelTool.js';
+let labelTool = new ImageLabelTool(document.querySelector('#image-area'));
 labelTool.add();
