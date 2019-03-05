4e3ca1835fa783e7e3fddf4048559b64d36544c5
songofhawk <songofhawk@gmail.com>
2019-03-05 15:13:43 +0800

    整理3DImageText的初始化代码,为生成文字做准备

--- src/cssgraph/Graph3DImageText.js
+++ src/cssgraph/Graph3DImageText.js
@@ -17,116 +17,133 @@ export class Graph3DImageText extends Graph{
         let imageObj = new Image();
         let self = this;
 
-        imageObj.onload = function() {
+        function initLocation(element) {
+            element.width = graphOption.realWidth;
+            element.height = graphOption.realHeight;
+            element.style.position = 'absolute';
+            element.style.left = graphOption.x + 'px';
+            element.style.top = graphOption.y + 'px';
+            element.style.marginLeft = -offsetX + 'px';
+            element.style.marginTop = -offsetY + 'px';
+        }
 
-            imageObj.width = graphOption.realWidth;
-            imageObj.height = graphOption.realHeight;
-            imageObj.style.position = 'absolute';
-            imageObj.style.left = graphOption.x+'px';
-            imageObj.style.top = graphOption.y+'px';
-            imageObj.style.marginLeft = - offsetX + 'px';
-            imageObj.style.marginTop = - offsetY  + 'px';
-
-
-            imageObj.show=function(){
+        function attachMethods(element) {
+            element.show = function () {
                 this.style.display = '';
             };
-            imageObj.hide=function(){
+            element.hide = function () {
                 this.style.display = 'none';
             };
-            imageObj.moveTo=function(x,y){
+            element.moveTo = function (x, y) {
                 let left = this.style.left;
                 let top = this.style.top;
-                this.style.left =  x  +'px';
-                this.style.top =  y + 'px';
+                this.style.left = x + 'px';
+                this.style.top = y + 'px';
             };
-            imageObj.moveLeftUp=function(distance){
+            element.moveLeftUp = function (distance) {
                 let left = this.style.left;
                 let top = this.style.top;
-                this.style.left =  parseInt(left.substring(0,left.indexOf('px'))) - distance +'px';
-                this.style.top =  parseInt(top.substring(0,top.indexOf('px'))) - distance + 'px';
+                this.style.left = parseInt(left.substring(0, left.indexOf('px'))) - distance + 'px';
+                this.style.top = parseInt(top.substring(0, top.indexOf('px'))) - distance + 'px';
             };
-            imageObj.resize=function(width, height){
+            element.resize = function (width, height) {
                 this.width = width;
                 this.height = height;
-                this.style.marginLeft = - width/2 + 'px';
-                this.style.marginTop = -height/2  + 'px';
+                this.style.marginLeft = -width / 2 + 'px';
+                this.style.marginTop = -height / 2 + 'px';
             };
-            imageObj.rotateX=function(x){
-                imageObj._setTransformOne(x,'X');
+            element.rotateX = function (x) {
+                imageObj._setTransformOne(x, 'X');
                 imageObj._composeStyleTransform();
             };
-            imageObj.rotateY=function(y){
-                imageObj._setTransformOne(y,'Y');
+            element.rotateY = function (y) {
+                imageObj._setTransformOne(y, 'Y');
                 imageObj._composeStyleTransform();
             };
-            imageObj.rotateZ=function(z){
-                imageObj._setTransformOne(z,'Z');
+            element.rotateZ = function (z) {
+                imageObj._setTransformOne(z, 'Z');
                 imageObj._composeStyleTransform();
             };
-            imageObj.rotateXY=function(x,y){
-                imageObj._setTransformOne(x,'X');
-                imageObj._setTransformOne(y,'Y');
+            element.rotateXY = function (x, y) {
+                imageObj._setTransformOne(x, 'X');
+                imageObj._setTransformOne(y, 'Y');
                 imageObj._composeStyleTransform();
             };
-            imageObj.rotateXYZ=function(x,y,z){
-                imageObj._setTransformOne(x,'X');
-                imageObj._setTransformOne(y,'Y');
-                imageObj._setTransformOne(z,'Z');
+            element.rotateXYZ = function (x, y, z) {
+                imageObj._setTransformOne(x, 'X');
+                imageObj._setTransformOne(y, 'Y');
+                imageObj._setTransformOne(z, 'Z');
                 imageObj._composeStyleTransform();
             };
-            imageObj._setTransformOne = function(value, name){
-                if (typeof(value) === "undefined" || value===null){
+            element._setTransformOne = function (value, name) {
+                if (typeof(value) === "undefined" || value === null) {
                     return false;
                 }
-                if (!imageObj.transform){
-                    imageObj.transform={}
+                if (!imageObj.transform) {
+                    imageObj.transform = {}
                 }
-                imageObj.transform['rotate'+name] = value;
+                imageObj.transform['rotate' + name] = value;
                 return true;
             };
-            imageObj._composeStyleTransform = function(){
+            element._composeStyleTransform = function () {
                 let trans = imageObj.transform;
-                let x = typeof(trans.rotateX)==='undefined' ? '' : ('rotateX('+trans.rotateX+'deg) ');
-                let y = typeof(trans.rotateY)==='undefined' ? '' : ('rotateY('+trans.rotateY+'deg) ');
-                let z = typeof(trans.rotateZ)==='undefined' ? '' : ('rotateZ('+trans.rotateZ+'deg) ');
-                this.style.transform = x+y+z;
+                let x = typeof(trans.rotateX) === 'undefined' ? '' : ('rotateX(' + trans.rotateX + 'deg) ');
+                let y = typeof(trans.rotateY) === 'undefined' ? '' : ('rotateY(' + trans.rotateY + 'deg) ');
+                let z = typeof(trans.rotateZ) === 'undefined' ? '' : ('rotateZ(' + trans.rotateZ + 'deg) ');
+                this.style.transform = x + y + z;
             };
 
+        }
 
+        function addToContainer() {
             imageObj.hide();
             let container = self._panel._container;
             container.appendChild(imageObj);
-            self._image = imageObj;
+            self._element = imageObj;
+        }
 
+        function createRect() {
             let rect = new Konva.Rect({
                 x: 0,
                 y: 0,
-                fillEnabled:true,
-                fill:Graph.DEFAULT_FILL_COLOR,
+                fillEnabled: true,
+                fill: Graph.DEFAULT_FILL_COLOR,
                 width: graphOption.realWidth,
                 height: graphOption.realHeight,
-                listening:true
+                listening: true
             });
             wrapper.add(rect);
 
-            if (graphOption.bindEvent){
+            if (graphOption.bindEvent) {
                 self._bindEvent(wrapper);
+            }
+        }
+
+        imageObj.onload = function() {
+            initLocation(imageObj);
+            attachMethods(imageObj);
+            addToContainer(imageObj);
+            if (graphOption.bindEvent){
                 self._bindImageEvent(imageObj);
             }
-            wrapper.on("dragstart",function () {
-                wrapper.listening(false);
-            });
-
-            wrapper.on("dragend",function () {
-                let pos = wrapper.getAbsolutePosition();
-                self._image.moveTo(pos.x, pos.y);
-                wrapper.listening(true);
-            })
+            createRect();
+            self._bindWrapperEvent(wrapper, self);
 
         };
         imageObj.src = graphOption.src;
 
+    }
+
+    _bindWrapperEvent(wrapper, self) {
+        wrapper.on("dragstart", function () {
+            wrapper.listening(false);
+        });
+
+        wrapper.on("dragend", function () {
+            let pos = wrapper.getAbsolutePosition();
+            self._element.moveTo(pos.x, pos.y);
+            wrapper.listening(true);
+        })
     }
 
     _bindImageEvent(imageObj){
@@ -153,7 +170,7 @@ export class Graph3DImageText extends Graph{
         this._graphWrapper.setX(screenPoint.x);
         this._graphWrapper.setY(screenPoint.y);
 
-        this._image.moveTo(screenPoint.x, screenPoint.y);
+        this._element.moveTo(screenPoint.x, screenPoint.y);
     }
 
     select(){
@@ -176,24 +193,24 @@ export class Graph3DImageText extends Graph{
 
     highlight(){
         //this._graphWrapper.decor.show();
-        this._image.hide();
+        this._element.hide();
         //this._graphWrapper.image.show();
         super.highlight();
     }
 
     unHighlight(){
         //this._graphWrapper.decor.hide();
-        this._image.show();
+        this._element.show();
         //this._graphWrapper.image.hide();
         super.unHighlight();
     }
 
     onDrawingOver(){
-        //this._image.moveLeftUp(1);
-        this._image.show();
+        //this._element.moveLeftUp(1);
+        this._element.show();
 
         this._bindEvent(this._graphWrapper);
-        this._bindImageEvent(this._image);
+        this._bindImageEvent(this._element);
 
 
         this.genAbsolutePosition();
@@ -205,13 +222,13 @@ export class Graph3DImageText extends Graph{
         this._graphWrapper.offsetX(this._graphWrapper.width()/2);
         this._graphWrapper.offsetY(this._graphWrapper.height()/2);
 
-        this._image.moveTo(this.x, this.y);
-        this._image.resize(this.realWidth, this.realHeight);
+        this._element.moveTo(this.x, this.y);
+        this._element.resize(this.realWidth, this.realHeight);
 
     }
 
     onRotate(rotationDegree){
-        this._image.rotateZ(rotationDegree);
+        this._element.rotateZ(rotationDegree);
         super.onRotate();
     }
 
@@ -287,7 +304,7 @@ export class Graph3DImageText extends Graph{
             let self = this;
             rotate3dHandler.on('dragmove',()=>{
                 //console.log('x:'+rotate3dHandler.x()+', y:'+rotate3dHandler.y());
-                self._image.rotateXY(rotate3dHandler.y(),rotate3dHandler.x());
+                self._element.rotateXY(rotate3dHandler.y(),rotate3dHandler.x());
             })
 
         }else{
